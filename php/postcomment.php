f4d3c2b6c54b01ca26ab2791e9bcd65241950101
chubbaymoo <nguyen.a.sn@gmail.com>
2016-05-08 16:58:30 -0700

    postcomment.php

--- php/postcomment.php
+++ php/postcomment.php
@@ -25,4 +25,38 @@
 	} catch(PDOException $ex) {
     echo 'ERROR: ' . $ex->getMessage();
 }
+
+/* STUFF FROM PROFILE.PHP
+function newComment(id){
+			var commentText = $('#commentbox').val();
+			if(!$.trim(commentText)) {
+				alert("Pleae enter a comment first!");
+				return;
+			}
+			else{
+			$.ajax({
+				url: 'php/postcomment.php',
+				type: 'POST',
+				data: {uid: id, comment: commentbox},
+				datatype: 'html',
+				success: function(data) {
+					var list = JSON.parse(data);
+					list.forEach(function(i) {
+					updateComments(i);
+				});
+				}
+			})
+		}
+
+		<div class="form-group text-right">
+							  <textarea class="form-control" rows="5" id="commentbox"></textarea>
+						</div>
+						
+						<div class="form-group last">
+							<div class="col-sm-offset-3 col-sm-9">
+								<button type="button" onclick="return newComment(<?php echo $_GET['id']; ?>)" class="btn btn-primary btn-sm">Post Comment</button>
+								<button type="reset" class="btn btn-default btn-sm">Reset</button>
+							</div>
+						</div>
+						*/
 ?>
